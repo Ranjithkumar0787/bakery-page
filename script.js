34ba093d3439efd29f4664837ebbb200f5d15964
ranjithkumar0787 <ranjithkumar380787@gmail.com>
2024-08-05 11:46:27 +0530

    added

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
       const cartCount = document.getElementById('cart-count');
       let cartItemCount = 0;
       cartButtons.forEach(button => {
-          button.addEventListener('click', () => {
-              cartItemCount++;
-              cartCount.textContent = cartItemCount;
-              alert('Added to cart!');
-          });
+            button.addEventListener('click', () => {
+                  cartItemCount++;
+                  cartCount.textContent = cartItemCount;
+                  alert('Added to cart!');
+            });
       });
-      });
+});
